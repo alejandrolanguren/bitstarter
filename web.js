db7645dc27256e2edaea6c427121bc4c4bdd3375
alejandrolanguren <alejandro.languren@gmail.com>
2013-07-12 17:38:54 -0500

    debug - 1st session

--- web.js
+++ web.js
@@ -1,20 +1,20 @@
 
-var fs = require("fs"); 
-var fileName = "index.html"; 
 
 var express = require('express');
 var app = express.createServer(express.logger());
 
-fs.readFile(fileName, function (err, data) {
-    if (err) throw err;
-    //console.log(data.toString());
-      app.get('/', function(request, response) {
-        response.send(data.toString());
-      });
+var fs = require("fs");
 
-    });
+fs.readFile("index.html", function (err, data) {
+    if (err) throw err;
+    console.log(data.toString());
+    var buffer = new Buffer(data, "utf-8")
+    console.log(buffer.toString());
 });
 
+app.get('/', function(request, response) {
+    response.send(buffer.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
