6d1d002b259186ae5a5931574fb2042a44142f7c
alejandrolanguren <alejandro.languren@gmail.com>
2013-07-12 17:19:03 -0500

    web.js updated to read html

--- web.js
+++ web.js
@@ -1,10 +1,20 @@
-var express = require('express');
 
+var fs = require("fs"); 
+var fileName = "index.html"; 
+
+var express = require('express');
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+fs.readFile(fileName, function (err, data) {
+    if (err) throw err;
+    //console.log(data.toString());
+      app.get('/', function(request, response) {
+        response.send(data.toString());
+      });
+
+    });
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
