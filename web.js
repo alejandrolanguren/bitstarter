8632b98303b28f4fdbb643aa1494bbd66646f410
alejandrolanguren <alejandro.languren@gmail.com>
2013-07-12 17:42:09 -0500

    debug - 2nd session

--- web.js
+++ web.js
@@ -1,14 +1,13 @@
-
-
 var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require("fs");
+var buffer = new Buffer(20);
 
 fs.readFile("index.html", function (err, data) {
     if (err) throw err;
     console.log(data.toString());
-    var buffer = new Buffer(data, "utf-8")
+    buffer = new Buffer(data, "utf-8")
     console.log(buffer.toString());
 });
 
